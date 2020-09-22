4660874529b3a88246379af687e9abf589046337
Kiril Piskunov <kiril@confluent.io>
2020-09-22 12:38:05 +0100

    fixed logging

--- shipment-transformation-xml/src/main/java/shipment/tracking/transformer/Transformer.java
+++ shipment-transformation-xml/src/main/java/shipment/tracking/transformer/Transformer.java
@@ -7,23 +7,27 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import shipment.tracking.transformer.xml.HawbDimensions;
 import shipment.tracking.transformer.xml.Tracking;
 import shipment.tracking.transformer.xml2JsonMappers.TrackingMapper;
 
 public class Transformer {
 
+  static Logger logger = LoggerFactory.getLogger(Transformer.class);
+
   static XmlMapper xmlMapper = new XmlMapper();
   static ObjectMapper objectMapper = new ObjectMapper();
 
   static shipment.tracking.transformer.xml.Tracking parseXml(String raw) {
     try {
-      System.out.println("INPUT: " + raw);
+      logger.debug("INPUT: " + raw);
 
       return xmlMapper.readValue(raw, Tracking.class);
     } catch (JsonProcessingException e) {
       //TODO what to do when data is not 'parsable'?
-      e.printStackTrace();
+      logger.warn("Error parsing XML: ", e);
       return null;
     }
   }
@@ -32,17 +36,19 @@ public class Transformer {
 
     try {
       //Handle embedded XML
-      String hawbDimensionsXmlString = shipment.getHawbDetails().getHawbDimensionsXmlString();
+      if (shipment != null && shipment.getHawbDetails() != null) {
+        String hawbDimensionsXmlString = shipment.getHawbDetails().getHawbDimensionsXmlString();
 
-      HawbDimensions hawbDimensions = xmlMapper.readValue(hawbDimensionsXmlString, HawbDimensions.class);
-      shipment.getHawbDetails().setHawbDimensions(hawbDimensions);
+        HawbDimensions hawbDimensions = xmlMapper.readValue(hawbDimensionsXmlString, HawbDimensions.class);
+        shipment.getHawbDetails().setHawbDimensions(hawbDimensions);
+      }
 
       shipment.tracking.transformer.json.Tracking trackingJsonFormat = TrackingMapper.INSTANCE.trackingXmlToJson(shipment);
 
       return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(trackingJsonFormat);
 
     } catch (JsonProcessingException e) {
-      e.printStackTrace();
+      logger.warn("Error parsing embedded XML: ", e);
       return null;
     }
   }
