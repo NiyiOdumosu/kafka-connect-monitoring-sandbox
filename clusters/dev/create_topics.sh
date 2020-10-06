034e857e7c2881488e44333008f40273c75fba7f
Kiril Piskunov <kiril@confluent.io>
2020-10-06 10:15:24 +0100

    script docs

--- clusters/dev/create_topics.sh
+++ clusters/dev/create_topics.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# This scripts creates topics based on a provided topology defintion file
+#
+# This scripts exepects to find kafka-topology-builder.jar file in the
+# same directory where it is running from.
+#
+# Required environment variables:
+# CCLOUD_BOOTSTRAP_SERVERS=hostname:port
+# CCLOUD_SASL_JAAS_CONFIG=org.apache.kafka.common.security.plain.PlainLoginModule   required username="username"   password="password";
+# TOPOLOGY_DEFINITION_YAML=/tmp/toplogy.yaml
+
+
+
 function finish {
   rm -rf ${RAND_ID}.ccloud.properties
 }
@@ -17,10 +29,16 @@ if [[ -z "${CCLOUD_SASL_JAAS_CONFIG}" ]]; then
 	exit 1
 fi
 
-if [[ ! -f kafka-topology-builder-jar-with-dependencies.jar ]]; then
+if [[ -z "${TOPOLOGY_DEFINITION_YAML}" || ! -f "${TOPOLOGY_DEFINITION_YAML}" ]]; then
+  echo "TOPOLOGY_DEFINITION_YAML env varaiable is undefined or YAML file does not exist. Exiting"
+  exit 1
+fi
+
+if [[ ! -f kafka-topology-builder.jar ]]; then
   echo "Can't find kafka-topology-builder jar. Exiting"
   exit 1
 fi
+
 
 cat << EOF > ./$RAND_ID.ccloud.properties
 security.protocol=SASL_SSL
@@ -30,10 +48,10 @@ sasl.mechanism=PLAIN
 request.timeout.ms=3000
 EOF
 
-java -jar kafka-topology-builder-jar-with-dependencies.jar \
+java -jar kafka-topology-builder.jar \
                 --brokers ${CCLOUD_BOOTSTRAP_SERVERS} \
                 --clientConfig ${RAND_ID}.ccloud.properties \
-                --topology ./topologies/aramex.yml
+                --topology ${TOPOLOGY_DEFINITION_YAML}
 
 
 
